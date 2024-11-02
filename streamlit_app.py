e8018f6f5cb4b13fd4b3ce5b820c94002baf602a
itsma8di <130710914+itsma8di@users.noreply.github.com>
2024-11-02 08:04:40 +0000

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ import pandas as pd
 import requests
 
 # labels
-labels = requests.get("http://localhost:5000/api/labels").json()
+labels = requests.get("https://blank-app-h4ahyp7v1fh.streamlit.app/api/labels").json()
 selector = st.multiselect("Select WELL:", labels)
 
 # load data
 data = pd.read_json(
-    requests.get("http://localhost:5000/api/data", params={"selector": selector}).json()
+    requests.get("https://blank-app-h4ahyp7v1fh.streamlit.app/api/data", params={"selector": selector}).json()
 )
 
 # setup figure
